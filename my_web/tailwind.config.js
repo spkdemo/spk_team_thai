71da9baec65ce742849757a8c39e34e013120965
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2024-12-28 00:06:07 +0700

    no message

---
+++ my_web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // ไฟล์หลัก HTML
+    "./src/**/*.{js,ts,jsx,tsx}", // ไฟล์ใน src ทุกประเภทที่เกี่ยวข้อง
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
