5e2de4148a8664a8620a87f88309bff48b8099a4
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2024-12-28 22:55:22 +0700

    Navbar

---
+++ demo_web/src/components/navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+// src/components/Navbar.tsx
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const Navbar = () => {
+  return (
+    <AppBar position="fixed" color="primary">
+      <Toolbar>
+        {/* เมนูไอคอน */}
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        {/* โลโก้หรือข้อความ */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          MyApp
+        </Typography>
+        {/* ปุ่มใน Navbar */}
+        <Box>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Contact</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
